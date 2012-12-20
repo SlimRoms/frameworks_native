e8c20d61668798782e78fda9e8832bdc9766c9b5
Arun Kumar K.R <akumarkr@codeaurora.org>
2012-12-19 18:10:46 -0800

    SurfaceFlinger: Change to support framebuffer flip for 2D blitters
    - Surfaceflinger does not flip framebuffers when there are
      no layers marked for HWC_FRAMEBUFFER
    - This change checks for the HWC_BLIT flag and will request a flip
      to a new FB_TARGET buffer even if there are no FRAMEBUFFER layers
    
    Change-Id: Ib045e562481c6447d2064fc9693057652b417479

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -1418,6 +1418,7 @@ void HWComposer::dump(String8& result) const {
                             "HWC",
                             "BACKGROUND",
                             "FB TARGET",
+                            "FB_BLIT",
                             "UNKNOWN"};
                     if (type >= NELEM(compositionTypeName))
                         type = NELEM(compositionTypeName) - 1;
