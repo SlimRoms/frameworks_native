edaab7963ad3fd50a9aa115fa17d428bc28494f8
Steve Kondik <shade@chemlab.org>
2014-02-18 21:18:05 -0800

    sf: Enable swaprect optimization by default only on QCOMs
     * This is confirmed to cause problems at least on i9500.
     * Only enable it by default on QCOMs, other devices can opt
       in using the system property.
    
    Change-Id: Ic00137201f4a5383877f4033473c1ea5ac32a3a7

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -85,6 +85,12 @@
 #include "SecTVOutService.h"
 #endif
 
+#ifdef ENABLE_SWAPRECT
+#define SWAPRECT_DEFAULT "1"
+#else
+#define SWAPRECT_DEFAULT "0"
+#endif
+
 #define DISPLAY_COUNT       1
 
 /*
@@ -196,7 +202,7 @@ SurfaceFlinger::SurfaceFlinger()
 #endif
 #endif
 
-    property_get("debug.sf.swaprect", value, "1");
+    property_get("debug.sf.swaprect", value, SWAPRECT_DEFAULT);
     mSwapRectEnable = atoi(value) ? true:false ;
 }
 
