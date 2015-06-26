0efef1b08cf409f117ba13d11456ee3287889190
Griffin Millender <griffinn.millender@gmail.com>
2015-06-26 17:18:19 -0500

    Add slim version to bugreporter
    Change-Id: Id0444cbd87f56491bdc305e7bde387ded5992f73

--- cmds/dumpstate/dumpstate.c
+++ cmds/dumpstate/dumpstate.c
@@ -105,7 +105,9 @@ static void dumpstate() {
     char radio[PROPERTY_VALUE_MAX], bootloader[PROPERTY_VALUE_MAX];
     char network[PROPERTY_VALUE_MAX], date[80];
     char build_type[PROPERTY_VALUE_MAX];
+    char slim_version[PROPERTY_VALUE_MAX];
 
+    property_get("ro.slim.version", slim_version, "(unknown)");
     property_get("ro.build.display.id", build, "(unknown)");
     property_get("ro.build.fingerprint", fingerprint, "(unknown)");
     property_get("ro.build.type", build_type, "(unknown)");
@@ -120,6 +122,7 @@ static void dumpstate() {
 
     printf("\n");
     printf("Build: %s\n", build);
+    printf("Slim Version: %s\n", slim_version);
     printf("Build fingerprint: '%s'\n", fingerprint); /* format is important for other tools */
     printf("Bootloader: %s\n", bootloader);
     printf("Radio: %s\n", radio);
