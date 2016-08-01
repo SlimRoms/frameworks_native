063c6f1a3212c4c76dd4d71878aa6271a682d885
Steve Kondik <steve@cyngn.com>
2016-08-01 13:21:58 -0700

    surfaceflinger: Isolate S3D code to msm8996 only
    Change-Id: Ie4b77b55eb306c795b452c59d8ae3383bd2804bc

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -126,6 +126,9 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_SHARED_LIBRARIES += libqdMetaData
     LOCAL_CFLAGS += -DQTI_BSP
+    ifeq ($(call is-board-platform-in-list,msm8996),true)
+        LOCAL_CFLAGS += -DSDM_TARGET
+    endif
 endif
 
 LOCAL_MODULE := libsurfaceflinger
