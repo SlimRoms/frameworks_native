1c6c4246eaa209c1a0aba17fd0d664bda097f7c2
Umesh Pandey <umeshp@codeaurora.org>
2016-09-28 23:41:31 -0700

    SurfaceFlinger: Use TARGET_OUT_HEADERS
    Include files from TARGET_OUT_HEADERS instead of
    absolute path.
    
    Change-Id: Iead2142a588e57f041a4778cb443aafaf06214f4
    Crs-fixed: 1074697

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -162,8 +162,7 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libgralloc
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libqdutils
   else
-    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
-    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
+    LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
   endif
   LOCAL_SHARED_LIBRARIES += libqdutils
   LOCAL_SHARED_LIBRARIES += libqdMetaData
